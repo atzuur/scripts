ecdb6a727561f10b7ff5c940db2e656af9a93a2a
atzur <99679220+atzuur@users.noreply.github.com>
2022-04-21 16:25:38 +0300

    Update reenc.bat

--- reenc.bat
+++ reenc.bat
@@ -5,18 +5,17 @@ mode con: lines=5
 powershell.exe -noexit -noprofile -command "$argv = $input | ?{$_}; iex (${%~f0} | out-string)"
 : end batch / begin powershell #>
 
-Get-ChildItem $argv | ForEach-Object { # Loops through each queued video
+Get-ChildItem $argv | ForEach-Object {
 
-    $round++ # Increments the round counter
+    $round++
 
-    $Host.UI.RawUI.WindowTitle = "[$($round)/$($argv.count)] Encoding $($_.Name)" # Sets the window title with progress indicator and the video that's currently gettign encoded
+    $Host.UI.RawUI.WindowTitle = "[$($round)/$($argv.count)] Encoding $($_.Name)"
 
-    [String]$Out = Join-Path $_.Directory ($_.BaseName + ' - reenc.mp4') # Setting up a variable for the output file name
+    [String]$Out = Join-Path $_.Directory ($_.BaseName + ' - reenc.mp4')
 
-    ffmpeg -i `"$($_.FullName)`" -loglevel warning -stats -c:v libx264 -c:a libopus -preset slow -x264-params aq-mode=3 -crf 19 -b:a 128k -pix_fmt yuv420p10le `"$($out)`" # Actually run the command
-    if ($LASTEXITCODE -ne 0){pause} # Pause if there's an error
+    ffmpeg -i `"$($_.FullName)`" -loglevel warning -stats -c:v libx264 -c:a libopus -preset slow -x264-params aq-mode=3 -crf 19 -b:a 128k -pix_fmt yuv420p10le `"$($out)`"
+    if ($LASTEXITCODE -ne 0){pause}
 
-    if ($round -ne $argv.count){Clear-Host} # Clear the screen until the last video comes through
-
+    if ($round -ne $argv.count){Clear-Host}
 }
 exit
